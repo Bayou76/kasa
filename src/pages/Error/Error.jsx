dc217727082df856bb0fe67725dfa8c54b44757e
bayous3 <sebia.baya@outlook.com>
2024-05-04 15:43:50 +0200

    page error

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,15 +1,27 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import './error.scss'
 
-function Error(){
-    return (
-        <div>
-            <div>
-               <h1>404</h1>
-            <p>Oup! La page que vous demandez n'existe pas.</p> 
-            </div>
-            <Link to="/">Retourner sur la page d'accueil</Link>
-        </div>
-    )
+function Error() {
+	return (
+		<div>
+			<Header />
+			<div className="error">
+				<div className="error_info">
+					<h1 className="error_info_title">404</h1>
+					<p className="error_info_content">
+						Oup! La page que vous demandez n'existe pas.
+					</p>
+				</div>
+				<Link className="error_info_return" to="/">
+					Retourner sur la page d'accueil
+				</Link>
+			</div>
+
+			<Footer />
+		</div>
+	)
 }
 
 export default Error
