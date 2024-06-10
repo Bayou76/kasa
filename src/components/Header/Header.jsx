0d03b4f40d13c120691d9f3328de08e394539bcd
bayous3 <sebia.baya@outlook.com>
2024-06-10 18:45:07 +0200

    maj Kasa

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,20 @@
-import Logo from '../../assets/logo.png'
-import Navbar from '../Navbar/Navbar'
-import './header.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
+import Navbar from '../Navbar/Navbar';
+import './header.scss';
 
 function Header() {
 	return (
 		<header className='header'>
 			<h1>
-				<img src={Logo} alt="kasa, location d'appartements" />
+				<Link to='/Kasa'>
+					<img src={Logo} alt="kasa, location d'appartements" />
+				</Link>
 			</h1>
 			<Navbar />
 		</header>
-	)
+	);
 }
 
-export default Header
+export default Header;
